0a83433199936f6d0adab958bda1d400fde40e5d
stef-the <75397677+stef-the@users.noreply.github.com>
2023-10-23 17:00:17 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<slot />
+
+<style>
+    :root {
+        --accent: #f00;
+        --accent-light: #faa;
+        --accent-dark: #500;
+
+        --main: #222;
+        --main-light: #333;
+        --main-dark: #111;
+
+        --main-text: #eee;
+
+        --accent-gradient: linear-gradient(45deg, var(--accent), var(--accent-light) 30%, white 60%);
+    }
+
+    :global(body) {
+        font-family: Helvetica, sans-serif;
+        color: var(--main-text);
+        background-color: var(--main);
+        overflow: hidden;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+    const cards = [
+        {
+            title: 'Why we <3 Gussy',
+            description: 'we don\'t',
+            image: 'edouard0.jpg',
+        },
+        {
+            title: 'Why Goonstave',
+            description: ':)',
+            image: 'gustave0.jpg',
+        }
+    ]
+</script>
+
+<div id="header">
+  <h1>Welcome to <b class="gradient">Gustave</b></h1>
+  <i>wear the world gets it's fun</i>
+</div>
+{#each cards as card}
+    <div class='card'>
+        <div class='cardimage' style='background-image: url("{card.image}");'></div>
+        <div>
+            <h2>{card.title}</h2>
+            <p>{card.description}</p>
+        </div>
+    </div>
+{/each}
+
+<style>
+  .gradient {
+    background-image: var(--accent-gradient);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 400%;
+  }
+  #header {
+    margin: 2.5rem;
+    width: 100%;
+    background-color: none;
+    text-align: center;
+  }
+  #header i {
+    font-size: 2rem;
+  }
+  h1, h2, p {
+    margin: 0;
+  }
+  h1 {
+    font-size: 5rem;
+  }
+  .card {
+    background-color: var(--main-dark);
+    margin: 2rem;
+    padding: 1rem;
+    height: 5rem;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    display: flex;
+  }
+  .card .cardimage {
+    height: 5rem;
+    width: 5rem;
+    border-radius: 0.5rem;
+    background-color: red;
+    margin-right: 1rem;
+  }
+  .card h2 {
+    margin-bottom: 0.5rem;
+  }
+</style>
